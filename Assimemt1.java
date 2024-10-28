5f8fcbfb2575b062c30fd2c1fc55059b8be40c13
KainSinford <154439341+KainSinford@users.noreply.github.com>
2024-10-28 08:54:17 +0700

    Add files via upload

---
+++ Assimemt1.java
@@ -0,0 +1,9 @@
+
+package assimemt.pkg1;
+public class Assimemt1 {
+    public static void main(String[] args) {
+        System.out.println("name,Truong Nguyen Bao Tam!");
+        System.out.println("Class， CMU-CS 303AGIS");
+        System.out.println("MSSV,29219045711");
+    }
+}
